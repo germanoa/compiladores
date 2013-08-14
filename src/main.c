098d9c81c9eab0e9dd198108de5d30cf4261baa0
Lucas Schnorr <Lucas.Schnorr@imag.fr>
2013-08-14 14:08:51 -0300

    get ready for step 2

--- src/main.c
+++ src/main.c
@@ -1,21 +1,22 @@
+/*
+   main.c
+
+   Arquivo principal do analisador sintático.
+*/
 #include <stdio.h>
 #include "comp_dict.h"
 #include "comp_list.h"
 #include "comp_tree.h"
 #include "comp_graph.h"
-#include "tokens.h"
 
-int getLineNumber (void)
+void yyerror (char const *mensagem)
 {
-  /* deve ser implementada */
-  return 0;
+  fprintf (stderr, "%s\n", mensagem);
 }
 
 int main (int argc, char **argv)
 {
-  int token = TOKEN_ERRO;
-  while (token = yylex()){
-    printf ("token <%d> at %d\n", token, getLineNumber());
-  }
-  return 0;
+  int resultado = yyparse();
+  return resultado;
 }
+
