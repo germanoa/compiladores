2a44d7469cbd982f79f7b6ff6954fe8856c19151
Artur Scheibler <artur.scheibler@gmail.com>
2013-08-14 20:32:42 -0300

    Adições para tree.

--- include/comp_tree.h
+++ include/comp_tree.h
@@ -14,13 +14,23 @@ static inline __comp_tree_init(comp_tree_t *tree);
 // create a tree, alloc mem and return address
 comp_tree_t *new_comp_tree();
 
+// set tree item with string
+void comp_tree_set_string(comp_tree_t *tree, const char *string);
+
+// print, depth first, entire tree contents as strings, separated by spaces
+void comp_tree_depth_print_string(comp_tree_t *tree);
+
 // alloc mem to child tree and append to children list
-void comp_tree_add_child(comp_tree_t *tree);
+// returns 1 if successful; 0 otherwise
+int comp_tree_create_child(comp_tree_t *tree);
+
+// create child and set its item with string
+void comp_tree_create_child_with_string(comp_tree_t *tree, const char *string);
 
 // delete child tree and remove from children list
 void comp_tree_remove_child(comp_tree_t *tree, comp_tree_t *child);
 
-// free address memory used by tree
+// free address memory used by tree and its subtrees
 void comp_tree_delete(comp_tree_t *tree);
 
 #endif /* COMP_TREE_H */
--- src/comp_tree.c
+++ src/comp_tree.c
@@ -2,6 +2,7 @@
 #include "comp_list.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 static inline __comp_tree_init(comp_tree_t *tree) {
 	tree->item = NULL;
@@ -22,40 +23,87 @@ comp_tree_t *new_comp_tree() {
     return tree;
 }
 
-void comp_tree_add_child(comp_tree_t *tree) {
+void comp_tree_set_string(comp_tree_t *tree, const char *string) {
 	if(tree == NULL) {
-		fprintf(stderr, "WARNING: Cannot add new child for null tree. Will autoallocate tree and add child.\n");
-		tree = new_comp_tree();
+		fprintf(stderr, "ERROR: Cannot set string for null tree.\n");
+		return;
+	}
+	
+	char *_string = malloc((strlen(string) + 1)*sizeof(char));
+	strcpy(_string, string);
+	tree->item = _string;
+}
+
+void comp_tree_depth_print_string(comp_tree_t *tree) {
+	// uses auxiliary function to print entire tree in a single line
+	// using recursion, but still printing a newline at the end
+	void __aux_comp_tree_depth_print_string(comp_tree_t *_tree) {
+		if(_tree == NULL)
+			return;
+			
+		printf("%s ", (char*)_tree->item);
+		
+		if(_tree->children != NULL) { // has children to print
+			comp_list_t *childList = _tree->children;
+			comp_tree_t *child = NULL;
+			
+			do { // runs through every child and calls recursion
+				child = childList->item;
+				
+				if(child == NULL)
+					fprintf(stderr, "WARNING: Tried to print child with no content. ");
+				else __aux_comp_tree_depth_print_string(child);
+				
+				childList = childList->next;
+			} while(childList != _tree->children);
+		}
+	}
+	
+	__aux_comp_tree_depth_print_string(tree);
+	printf("\n");
+}	
+
+int comp_tree_create_child(comp_tree_t *tree) {
+	if(tree == NULL) {
+		fprintf(stderr, "ERROR: Cannot create child for null tree.\n");
+		return 0;
 	}
 	
 	if(tree->children == NULL) {
 		tree->children = new_comp_list();
 		if(tree->children == NULL) {
-			fprintf(stderr, "ERROR: Error adding child.\n");
-			return;
+			fprintf(stderr, "ERROR: Error creating list entry for child.\n");
+			return 0;
 		}
 		
 		tree->children->item = new_comp_tree();
 		if(tree->children->item == NULL) {
-			fprintf(stderr, "ERROR: Error adding child.\n");
-			return;
+			fprintf(stderr, "ERROR: Error creating child.\n");
+			return 0;
 		}
 	}
-	else {
+	else { // tree already has children
 		comp_list_t *child = new_comp_list();
 		if(child == NULL) {
-			fprintf(stderr, "ERROR: Error adding child.\n");
-			return;
+			fprintf(stderr, "ERROR: Error creating list entry for child.\n");
+			return 0;
 		}
 		
 		child->item = new_comp_tree();
 		if(child->item == NULL) {
-			fprintf(stderr, "ERROR: Error adding child.\n");
-			return;
+			fprintf(stderr, "ERROR: Error creating child.\n");
+			return 0;
 		}
 		
 		comp_list_append(tree->children, child);
 	}
+	
+	return 1;
+}
+
+void comp_tree_create_child_with_string(comp_tree_t *tree, const char *string) {
+	if(comp_tree_create_child(tree))
+		comp_tree_set_string((comp_tree_t*)(tree->children->item), string);
 }
 
 void comp_tree_remove_child(comp_tree_t *tree, comp_tree_t *child) {
