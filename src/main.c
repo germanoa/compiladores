a982b793f15e0c2f97122295570b93888db36e27
Artur Scheibler <artur.scheibler@gmail.com>
2013-08-14 20:40:59 -0300

    Adições para tree.

--- src/main.c
+++ src/main.c
@@ -13,10 +13,31 @@ int getLineNumber (void)
 
 int main (int argc, char **argv)
 {
+	/*
     int token = TOKEN_ERRO;
     while (token = yylex()){
         printf ("token <%d> at %d\n", token, getLineNumber());
     }
-
+	*/
+	
+	comp_tree_t *tree = new_comp_tree();
+	printf("New tree.\n");
+	comp_tree_set_string(tree, "1");
+	puts(tree->item);
+	
+	comp_tree_create_child(tree);
+	comp_tree_set_string(tree->children->item, "2");
+	puts(((comp_tree_t*)((tree->children)->item))->item);
+	
+	comp_tree_create_child(tree);
+	comp_tree_set_string(tree->children->next->item, "4");
+	puts(((comp_tree_t*)((tree->children->next)->item))->item);
+	
+	comp_tree_create_child(tree->children->item);
+	comp_tree_set_string(((comp_tree_t*)((tree->children)->item))->children->item, "3");
+	puts(((comp_tree_t*)((((comp_tree_t*)((tree->children))->item))->children)->item)->item);
+	
+	comp_tree_depth_print_string(tree);
+	printf("Out.\n");
     return 0;
 }
