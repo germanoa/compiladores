e617839deca7e55503526ed808d0e53c8a778560
Germano Andersson <germanoa@gmail.com>
2013-08-21 00:14:33 -0300

    scanner.l sem erros.

--- build/testando-etapa1.sh
+++ build/testando-etapa1.sh
@@ -2,5 +2,5 @@
 
 cd src/tests/tokens
 
-for i in `seq 0 15`; do cat input/nivel$i.tokens | ../../../build/test | nossa-saida/nivel$i.saida; done
+for i in `seq 0 15`; do cat input/nivel$i.tokens | ../../../build/test > nossa-saida/nivel$i.saida; done
 for i in `seq 0 15`; do diff nossa-saida/nivel$i.saida saida-esperada/nivel$i.saida > diffs/nivel$i.saida ; done
