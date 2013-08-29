b69f67ea0d2c1fa7b87c53be1780f50552604bdf
Germano Andersson <germanoa@gmail.com>
2013-08-29 09:06:37 -0300

    etapa2 - outra entrada de teste

---
+++ input/etapa2/etapa2-3.c
@@ -0,0 +1,25 @@
+/*
+    um comentario
+*/
+
+string : teste;
+char : aa;
+int: num; // comentario
+bool: valido;
+
+// comentario
+
+bool : funcao ()
+{
+    output 0;
+}
+
+
+int : main (int : argc, char : argv)
+  int : resultado; 
+  bool: res;
+{
+  res = funcao();
+  resultado = yyparse();
+  output resultado;
+}
