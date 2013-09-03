148d968c27c638b04eeb74ab7d4e257cd0cb6d4c
Lucas Schnorr <schnorr@inf.ufrgs.br>
2013-09-03 00:12:37 -0300

    new module to output a graph considering the AST nodes

---
+++ include/gv.h
@@ -0,0 +1,13 @@
+/*
+  gv.h
+
+  Funções públicas do módulo gv. Verifique a implementação para
+  maiores detalhes de como utilizar cada uma. 
+*/
+#ifndef __GV_H__
+#define __GV_H__
+void gv_init (const char *filename);
+void gv_close (void);
+void gv_declare (const int tipo, const void *pointer, const char *name);
+void gv_connect (const void *p1, const void *p2);
+#endif
