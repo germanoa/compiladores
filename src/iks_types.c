153512f9844d81a1c0c8f660928c98cab599a0b0
Germano Andersson <germanoa@gmail.com>
2013-10-06 16:36:37 -0300

    insert comments

--- src/iks_types.c
+++ src/iks_types.c
@@ -15,27 +15,27 @@ int verify_coercion(comp_tree_t *id, comp_tree_t *expr) {
   exprs = exprn->symbol;
   if (exprs) { //gambi because call function has no symbol
     if((ids->iks_type == IKS_INT)&&(exprs->iks_type == IKS_BOOL)) {
-      printf("coercion int to bool\n");
+      //printf("coercion int to bool\n");
       idn->need_coercion=IKS_COERCION_INT_TO_BOOL;
     }
     else if((ids->iks_type == IKS_INT)&&(exprs->iks_type == IKS_FLOAT)) {
-      printf("coercion int to float\n");
+      //printf("coercion int to float\n");
       idn->need_coercion=IKS_COERCION_INT_TO_FLOAT;
     }
     else if((ids->iks_type == IKS_FLOAT)&&(exprs->iks_type == IKS_BOOL)) {
-      printf("coercion float to bool\n");
+      //printf("coercion float to bool\n");
       idn->need_coercion=IKS_COERCION_FLOAT_TO_BOOL;
     }
     else if((ids->iks_type == IKS_FLOAT)&&(exprs->iks_type == IKS_INT)) {
-      printf("coercion float to int\n");
+      //printf("coercion float to int\n");
       idn->need_coercion=IKS_COERCION_FLOAT_TO_INT;
     }
     else if((ids->iks_type == IKS_BOOL)&&(exprs->iks_type == IKS_INT)) {
-      printf("coercion bool to int\n");
+      //printf("coercion bool to int\n");
       idn->need_coercion=IKS_COERCION_BOOL_TO_INT;
     }
     else if((ids->iks_type == IKS_BOOL)&&(exprs->iks_type == IKS_FLOAT)) {
-      printf("coercion bool to float\n");
+      //printf("coercion bool to float\n");
       idn->need_coercion=IKS_COERCION_BOOL_TO_FLOAT;
     }
     else if((exprs->iks_type == IKS_CHAR)) {
