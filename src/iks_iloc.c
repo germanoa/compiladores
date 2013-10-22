33aa91814000f9fd166a6ff039d9005b4bdbdd44
Germano Andersson <germanoa@gmail.com>
2013-10-22 03:07:23 -0200

    code generator skel.

--- src/iks_iloc.c
+++ src/iks_iloc.c
@@ -1,4 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "iks_iloc.h"
+#include "iks_ast.h"
+#include "comp_tree.h"
 
+
+void code_generator(comp_tree_t *ast) {
+	iks_ast_node_value_t *n;
+	n = ast->item;
+	switch(n->type) {
+		case IKS_AST_PROGRAMA:
+		case IKS_AST_FUNCAO:
+		case IKS_AST_IF_ELSE:
+		case IKS_AST_DO_WHILE:
+		case IKS_AST_WHILE_DO:
+		case IKS_AST_INPUT:
+		case IKS_AST_OUTPUT:
+		case IKS_AST_ATRIBUICAO:
+		case IKS_AST_RETURN:
+		case IKS_AST_BLOCO:
+		case IKS_AST_IDENTIFICADOR:
+		case IKS_AST_LITERAL:
+		case IKS_AST_ARIM_SOMA:
+		case IKS_AST_ARIM_SUBTRACAO:
+		case IKS_AST_ARIM_MULTIPLICACAO:
+		case IKS_AST_ARIM_DIVISAO:
+		case IKS_AST_ARIM_INVERSAO:
+		case IKS_AST_LOGICO_E:
+		case IKS_AST_LOGICO_OU:
+		case IKS_AST_LOGICO_COMP_DIF:
+		case IKS_AST_LOGICO_COMP_IGUAL:
+		case IKS_AST_LOGICO_COMP_LE:
+		case IKS_AST_LOGICO_COMP_GE:
+		case IKS_AST_LOGICO_COMP_L:
+		case IKS_AST_LOGICO_COMP_G:
+		case IKS_AST_LOGICO_COMP_NEGACAO:
+		case IKS_AST_VETOR_INDEXADO:
+		case IKS_AST_CHAMADA_DE_FUNCAO:
+		case IKS_AST_INDEFINIDO:
+		default:
+			break;
+	}
+}
